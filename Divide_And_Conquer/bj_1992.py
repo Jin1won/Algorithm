82fb0644cc38a70f9633368dbcb4a1c71eafa976
Jin1won <wlsdnjs16521@gmail.com>
2021-07-31 23:53:15 +0900

    백준 1992번 쿼드트리

---
+++ Divide_And_Conquer/bj_1992.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+video = []
+
+for i in range(n):
+    video.append(list(map(int,sys.stdin.readline().strip())))
+
+def compression(y , x, size): #그래프의 좌표 값과 그래프의 사이즈를 파라미터로 설정
+    for i in range(y, y+size): #2개의 for문을 사용해 (0,0)부터 (n,n)까지 탐색
+        for j in range(x, x+size): 
+            if video[y][x] != video[i][j]: #만약 (0,0)지점의 그래프 값과 탐색하려는 지점의 그래프 값이 다르면 한 번호로 합칠 수 없기 때문에 상하좌우 재귀함수를 호출
+                print("(",end='')
+                compression(y, x, size//2)
+                compression(y,(x+size//2),size//2)
+                compression((y+size//2),x,size//2)
+                compression((y+size//2),(x+size//2),size//2)
+                print(")",end='')
+                return
+    print(video[y][x],end='')
+
+compression(0,0,n)
--- Divide_And_Conquer/bj_5904.py
+++ Divide_And_Conquer/bj_5904.py
@@ -1,2 +1,25 @@
 import sys
 
+n = int(sys.stdin.readline().strip())
+
+def S(idx, size, stage):
+    if (stage == 0):
+        if (n == idx): 
+            return 'm'
+        return
+
+    S(idx, (size - stage - 3) // 2, stage - 1)
+    if ((idx + (size - stage - 3) // 2) == n): 
+        return 'm'
+    S(idx + ((size - stage - 3) // 2) + stage + 3, (size - stage - 3) / 2, stage - 1)
+
+
+size, stage = 3, 0
+while (n > size):
+    stage +=1 
+    size = size * 2 + stage + 3;
+
+if S(1, size, stage) == None:
+    print('o')
+else:
+    print('m')
